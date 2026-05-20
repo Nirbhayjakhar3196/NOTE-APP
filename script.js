2709a67e1dc129ede3fa6988bb0067c1f310b413
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-05-20 15:03:34 +0530

    feat:Added local Storage in my app

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+let notes = []
+
 const noteInput = document.getElementById('noteInput')
 const addbtn = document.getElementById('addbtn')
 const noteContainer = document.getElementById('noteContainer')
 
-function addNote() {
+function addNote(noteText, isNew = true) {
     
-    const storedNote = noteInput.value
+    const storedNote = noteText || noteInput.value
 
     if(storedNote.trim() == ""){
         return 
@@ -22,12 +24,19 @@ function addNote() {
 
     p.textContent = storedNote
     
+    if(isNew) {
+        notes.push(storedNote)
+        storeNotes()
+    }
+
     div.appendChild(p)
 
     div.appendChild(deleteBtn)
 
     deleteBtn.addEventListener('click' , () => {
+        notes.splice(notes.indexOf(storedNote) , 1)
         div.remove()
+        storeNotes()
     })
 
     noteContainer.appendChild(div)
@@ -41,6 +50,23 @@ function addNote() {
 addbtn.addEventListener('click' , () => {
     addNote()
 })
+
+function storeNotes(){
+    localStorage.setItem('notes' , JSON.stringify(notes))
+}
+
+function loadNotes(){
+    const storedNotes = JSON.parse(localStorage.getItem('notes'))
+    notes = storedNotes || []
+    if(storedNotes){
+        storedNotes.forEach((note) => {
+            addNote(note, false)
+        });
+    }
+}
+
+loadNotes()
+
 
 
 
